984106f85c0ad6f374fe0da69a898f7b2288216d
Chai14 <chaitanyadua2014@gmail.com>
2023-12-29 18:16:52 +0530

    signup continues

--- Services/forms.py
+++ Services/forms.py
@@ -4,3 +4,9 @@ from .models import Custom
 class CustomSection(forms.Form):
     demo = forms.CharField()
     email = forms.EmailField()
+
+
+class LoginPage(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    address = forms.CharField()
